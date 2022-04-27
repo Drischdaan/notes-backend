225ce79ba4138f275117abd985d3a3580faeab2d
Drischdaan <lpboy100@gmail.com>
2022-04-27 15:36:17 +0200

    Added health controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { cacheOptions } from './_config/cache.config';
 import { configOptions } from './_config/config';
 import { typeormConfig } from './_config/typeorm.config';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(configOptions),
     CacheModule.registerAsync(cacheOptions),
     TypeOrmModule.forRootAsync(typeormConfig),
+    HealthModule,
   ],
   controllers: [],
   providers: [
